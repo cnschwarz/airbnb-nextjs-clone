5c47170f0cbf13c1fb32f74ca7e4eca9bbd2aaff
Colin Schwarz <cnschwarz@users.noreply.github.com>
2021-12-03 20:23:30 +0100

    feat: add detail page for specific house item

--- components/House.js
+++ components/House.js
@@ -1,12 +1,23 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 export default function House(props) {
-    console.log(props);
     return (
-        <div>
-            <img src={props.picture} width="100%" alt="House picture" />
+        <Link href="/houses/[id]" as={'houses/' + props.id}>
+            <a>
+                <Image
+                    alt="House picture"
+                    src={props.picture}
+                    layout="responsive"
+                    objectFit="cover"
+                    width={6}
+                    height={4}
+                />
             <p>
                 {props.type} - {props.town}
             </p>
             <p>{props.title}</p>
-        </div>
+            </a>
+        </Link>
     )
 }
---
+++ pages/houses/[id].js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import houses from '../../houses.js'
+import Image from 'next/image'
+
+export default function House(props) {
+    return (
+        <div>
+            <Head>
+                <title>{props.house.title}</title>
+            </Head>
+            <Image
+                alt="House picture"
+                src={props.house.picture}
+                layout="responsive"
+                objectFit="cover"
+                width={6}
+                height={4} />
+            <p>
+                {props.house.type} - {props.house.town}
+            </p>
+            <p>{props.house.title}</p>
+        </div>
+    )
+}
+
+export async function getServerSideProps({ query }) {
+    const { id } = query
+
+    return {
+        props: {
+            house: houses.filter((house) => house.id === parseInt(id))[0]
+        }
+    }
+}
